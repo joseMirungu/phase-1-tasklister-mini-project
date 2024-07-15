01d81383cba216ee08aeac4191249beccfd2ad3f
joseMirungu <josephirungu890@gmail.com>
2024-07-15 03:03:31 +0300

    complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.getElementById("create-task-form");
+  const taskList = document.getElementById("tasks");
+
+  form.addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    const taskInput = document.getElementById("new-task-description");
+    const taskText = taskInput.value.trim(); 
+        if (taskText !== "") {
+   
+      const taskItem = document.createElement("li");
+      taskItem.textContent = taskText;
+
+     
+      const deleteButton = document.createElement("button");
+      deleteButton.textContent = "X";
+      deleteButton.addEventListener("click", function() {
+        taskItem.remove(); 
+      });
+
+      taskItem.appendChild(deleteButton); 
+      taskList.appendChild(taskItem); 
+
+      form.reset();
+        }
+  });
 });
